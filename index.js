07d3e7520a653096a126c1ad99898e1a2c07b117
Dhruva Maheshwari <maheshwaridhruva0@gmail.com>
2026-05-28 13:30:20 +0530

    add date and time

--- index.js
+++ index.js
@@ -4,7 +4,7 @@ const app = express()
 const port = 4000
 
 app.get('/' , (req  ,res) => {
-    res.send(`Hello my name is dhruva Maheshwari and today date is ${Date.now()}`)
+    res.send(`Hello my name is dhruva Maheshwari and today date is ${Date.now().toLocaleString()}`)
 })
 
 app.listen(port , ()=> console.log(`server is listen on port ${port}`))
